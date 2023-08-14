fec3eb255fb2905dc8126a5884a70d54d5fc3137
MartinLingeri <mlingeri@frba.utn.edu.ar>
2023-08-13 23:42:50 -0300

    graficos implementados

--- components/ProductChart.tsx
+++ components/ProductChart.tsx
@@ -1,32 +1,46 @@
 "use client"
 
-import { useState, useEffect } from "react"
-import { Line } from "react-chartjs-2"
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
+  CartesianGrid,
   Legend,
-} from "chart.js"
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  LineElement,
-  PointElement,
-  Title,
+  Line,
+  LineChart,
   Tooltip,
-  Legend
-)
+  XAxis,
+  YAxis,
+} from "recharts"
 
-export default function ProductChart(data:any) {
-    console.log(data)
+type Data = {
+  name: string
+  price: number
+}
 
-    return (
-        <Line data={data}></Line>
-    )
+type Props = {
+  data: Data[]
+}
+
+export default function ProductChart(props: Props) {
+  return (
+    <>
+      <LineChart width={600} height={300} data={props.data}>
+        <Line dataKey="Precio" stroke="#f33" strokeWidth={3} />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+      <LineChart width={600} height={300} data={props.data}>
+        <Line dataKey="Precio_a_dolar_oficial" stroke="#3a3" strokeWidth={3} />
+        <Line dataKey="Precio_a_dolar_blue" stroke="#33a" strokeWidth={3} />
+        <Line dataKey="Dolar_oficial" stroke="#3a38" strokeWidth={3} />
+        <Line dataKey="Dolar_blue" stroke="#33a8" strokeWidth={3} />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </>
+  )
 }
