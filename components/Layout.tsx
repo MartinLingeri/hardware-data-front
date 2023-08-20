c7d357a75cf196cb17aa979b2d584ef30e585ed8
MartinLingeri <mlingeri@frba.utn.edu.ar>
2023-08-19 22:53:51 -0300

    agregado link a home

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,19 @@
 import React from "react"
 import Search from "./Search"
 import Categories from "./Categories"
+import Link from "next/link"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="bg-gray-100 py-8 min-h-screen">
       <div className="mx-auto max-w-4xl">
+        <div className="flex items-center justify-center w-full mb-9">
+          <Link href="/" className="inline-block">
+            <h1 className="text-black text-2xl font-extrabold text-center">
+              Precios de Hardware
+            </h1>
+          </Link>
+        </div>
         <Search />
         <Categories />
         {children}
